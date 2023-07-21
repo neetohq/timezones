0779bce7b5533427e930db08285ad06df00c6939
Jijin K Haridas <jijin.haridas@bigbinary.com>
2023-07-21 17:39:28 +0530

    Implemented grouped options for select component (#12)

--- js-timezone-selector-plugin/src/selector/utils.jsx
+++ js-timezone-selector-plugin/src/selector/utils.jsx
@@ -1,12 +1,11 @@
 import { h } from 'htm/preact';
 
-export const createOptionButton = ({
-  key, value, onClick, label,
-}) => (
-  <button onClick={onClick} className="px-4 py-4 text-xs border-b" key={key} value={value}>
-    {label}
-  </button>
-);
+import {
+  DUMMY_TIMEZONES_ASIA,
+  DUMMY_TIMEZONES_AUSTRALIA,
+  DUMMY_TIMEZONES_EUROPE,
+  DUMMY_TIMEZONES_USA_CANADA,
+} from './constants';
 
 export const handleSelect = (e) => {
   const changeTimezoneButton = document.getElementById('changeTimezoneButton');
@@ -15,3 +14,37 @@ export const handleSelect = (e) => {
   const optionsContainer = document.getElementById('optionsContainer');
   optionsContainer.classList.toggle('hidden');
 };
+
+export const createOptionButton = ({
+  key, value, onClick, label,
+}) => (
+  <button onClick={onClick} className="px-4 py-4 text-xs border-y" key={key} value={value}>
+    {label}
+  </button>
+);
+
+export const createGroupedOptionButton = (group) => (
+  <div>
+    {group.map((groupItem, groupIndex) => {
+      const key = Object.keys(groupItem)[0];
+      const element = groupItem[key];
+      return (
+        <div className="flex flex-col p-4 border-b" key={groupIndex}>
+          <span className="px-4 text-base font-bold uppercase">{key}</span>
+          {element.map((timezone, index) => createOptionButton({
+            onClick: handleSelect, key: index, value: timezone, timezone, label: timezone,
+          }))}
+        </div>
+      );
+    })}
+  </div>
+);
+
+export const groupedOptions = [
+  { ASIA: DUMMY_TIMEZONES_ASIA },
+  { EUROPE: DUMMY_TIMEZONES_EUROPE },
+  { 'USA/CANADA': DUMMY_TIMEZONES_USA_CANADA },
+  { AUSTRALIA: DUMMY_TIMEZONES_AUSTRALIA },
+];
+
+export const DEFAULT_VALUE = groupedOptions[0].ASIA[0];
