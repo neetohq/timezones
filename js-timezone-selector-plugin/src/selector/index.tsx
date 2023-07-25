d961ed71b31a1e8b837a3f56df73e4267fea413d
Jijin K Haridas <jijin.haridas@bigbinary.com>
2023-07-25 12:10:29 +0530

    Fixed search box

--- js-timezone-selector-plugin/src/selector/index.tsx
+++ js-timezone-selector-plugin/src/selector/index.tsx
@@ -15,7 +15,7 @@ const Selector = () => {
   addOutsideClickListener();
 
   return (
-    <div class="flex flex-col p-4 w-72" id="timezoneSelector">
+    <div class="flex flex-col p-4 w-96" id="timezoneSelector">
       <Button />
       <OptionsContainer />
     </div>
