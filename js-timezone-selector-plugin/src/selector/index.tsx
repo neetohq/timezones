67491aa60c15066d42ffdd235611e80692b6a54a
Jijin K Haridas <jijin.haridas@bigbinary.com>
2023-07-26 16:38:06 +0530

    Added position prop

--- js-timezone-selector-plugin/src/selector/index.tsx
+++ js-timezone-selector-plugin/src/selector/index.tsx
@@ -5,9 +5,10 @@ import Button from "./Button";
 import OptionsContainer from "./OptionsContainer";
 import { DEFAULT_VALUE } from './utils';
 
-const Selector = () => {
+const Selector = ({ className = "", position = "bottom" }) => {
   const [selectedValue, setSelectedValue] = useState(DEFAULT_VALUE);
   const [showOptions, setShowOptions] = useState(false);
+  const top = position === "top";
 
   const addOutsideClickListener = () => {
     window.addEventListener('click', function (e) {
@@ -17,14 +18,20 @@ const Selector = () => {
     });
   };
 
+
   addOutsideClickListener();
 
   return (
-    <div class="flex flex-col p-4" id="timezoneSelector">
+    <div className={`flex flex-col p-4 relative w-full ${className}`} id="timezoneSelector">
       <Button showOptions={showOptions} setShowOptions={setShowOptions} selectedValue={selectedValue} />
-      {showOptions === true &&
-        (<OptionsContainer selectedValue={selectedValue} setSelectedValue={setSelectedValue} setShowOptions={setShowOptions} />)
-      }
+      {showOptions === true && (
+        <OptionsContainer
+          selectedValue={selectedValue}
+          setSelectedValue={setSelectedValue}
+          setShowOptions={setShowOptions}
+          className={`absolute ${top ? 'bottom-4' : 'top-4'} left-2 right-2`}
+        />
+      )}
     </div>
   )
 }
