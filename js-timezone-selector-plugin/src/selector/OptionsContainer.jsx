b97d178be7ed0d71af9affe6292aef560a1b6a47
Jijin K Haridas <jijin.haridas@bigbinary.com>
2023-07-26 16:38:55 +0530

    Added position prop (#53)

--- js-timezone-selector-plugin/src/selector/OptionsContainer.jsx
+++ js-timezone-selector-plugin/src/selector/OptionsContainer.jsx
@@ -4,11 +4,16 @@ import { useState } from 'preact/hooks';
 import Search from './Search';
 import Options from './Options';
 
-function OptionsContainer({ selectedValue, setSelectedValue, setShowOptions }) {
+function OptionsContainer({
+  className, selectedValue, setSelectedValue, setShowOptions,
+}) {
   const [searchInput, setSearchInput] = useState('');
 
   return (
-    <div className="flex flex-col text-gray-700 bg-white border border-gray-300 rounded shadow" id="optionsContainer">
+    <div
+      className={`w-full p-4 flex flex-col text-gray-700 bg-white border border-gray-300 rounded shadow ${className}`}
+      id="optionsContainer"
+    >
       <Search
         searchInput={searchInput}
         setSearchInput={setSearchInput}
