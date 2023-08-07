fef2cd638ec85390a80d05d02840f2723bc17a8e
Jijin K Haridas <jijin.haridas@bigbinary.com>
2023-08-07 21:42:53 +0530

    Added ianaTimezoneToHumanReadable function and updated structure

--- js/vite.config.ts
+++ js/vite.config.ts
@@ -14,11 +14,27 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   build: {
     emptyOutDir: true,
-
     lib: {
-      entry: path.resolve(__dirname, "./src/main.tsx"),
-      formats: ["iife", "es"],
+      entry: [
+        path.resolve(__dirname, "./index.tsx")
+      ],
+      formats: ["es"],
       name: "NeetoTimezoneSelector",
+      /*
+      TODO implement different named outputs:
+      dist/index.es.js
+      dist/react.es.js
+
+      // package.json
+      exports: {
+        "index": "./dist/index.es.js",
+        "react": "./dist/react.es.js"
+      }
+
+      Target goal in host app:
+      import NeetoTimezoneSelector from "neeto-timezones"
+      import NeetoTimezoneSelector from "neeto-timezones/react"
+      */
       fileName: (format) => `index.${format}.js`,
     },
     target: "modules",
