f4a679c03f7f23fe692f17c3f452c7ab70c19eb3
Jijin K Haridas <jijin.haridas@bigbinary.com>
2023-08-11 14:50:59 +0530

    Refactoring and UI enhancements (#107)

---
+++ js/timezone-selector-plugin/selector/commons/Button.jsx
@@ -0,0 +1,71 @@
+import { h } from "preact";
+
+function Button({
+  children,
+  customClass = "",
+  hideCitiesTooltip = false,
+  id = "",
+  key = "",
+  toolTipPosition = "bottom",
+  timezone,
+  onClick,
+}) {
+  const {
+    label, keywords: value, isDST, cities,
+  } = timezone;
+
+  const getCurrentTimeInTimezone = (timeZone) => {
+    const currentTimeInUTC = new Date();
+
+    const options = {
+      timeZone,
+      hour12: true,
+      hour: "numeric",
+      minute: "numeric",
+    };
+
+    const currentTimeInTimezone = currentTimeInUTC.toLocaleString(
+      "en-US",
+      options,
+    );
+
+    return currentTimeInTimezone;
+  };
+
+  return (
+    <button
+      {... {
+        id, key, onClick, value,
+      }}
+      className={`flex items-center gap-2 py-2 text-md ${customClass}`}
+      type="submit"
+    >
+      <span className="min-w-0 text-left break-words grow">
+        {hideCitiesTooltip
+          ? (label) : (
+            <span
+              data-tooltip={cities}
+              data-tooltip-position={toolTipPosition}
+              value={value}
+            >
+              {label}
+            </span>
+          )}
+
+        {isDST && (
+          <span
+            data-tooltip="Daylight savings Time applicable"
+            data-tooltip-position={toolTipPosition}
+            value={value}
+          >
+            &#127774;
+          </span>
+        )}
+      </span>
+      <span className="flex items-center shrink-0">{getCurrentTimeInTimezone(timezone.utc[0])}</span>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
